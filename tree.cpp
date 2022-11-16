f5ff304f1b8324430789ede6ae948556ddf43893
Diesyder <90964855+Diesyder@users.noreply.github.com>
2022-11-16 22:50:54 +0800

    updata

--- tree.cpp
+++ tree.cpp
@@ -11,28 +11,36 @@
 
 int main()
 {
-  /**/
+  /*变量定义*/
   BiTree T;//新建根结点
-  printf("<=========================[二叉树的部分基础操作]=========================>\n");
-  printf("请按先序输入二叉树(如：AB00C00表示A为根结点、B为左孩子、C为右孩子的二叉树，其中0代表空格)：\n");
+
+  /*二叉树创建*/
+  printf("<=========================[二叉树的创建]=========================>\n");
+  printf("[?]例：AB00C00表示A为根结点、B为左孩子、C为右孩子的二叉树，0代表空格\n");
+  printf("[@]请按先序输入二叉树：");
   CreateBiTree(T); // 建立二叉树T
   printf("\n");
 
-  printf("先序遍历为：");
+  /*二叉树深度遍历*/
+  printf("<=======================[二叉树的深度遍历]=======================>\n");
+  printf("|先序遍历为：");
   PreOrderTraverse(T,visit);
-  printf("\n中序遍历为：");
+  printf("\n|中序遍历为：");
   InOrderTraverse(T,visit);
-  printf("\n后序遍历为：");
+  printf("\n|后序遍历为：");
   PostOrderTraverse(T,visit);
   printf("\n\n");
 
+  /*二叉树目录输出*/
+  printf("<=======================[二叉树的目录输出]=======================>\n");
   printf("按目录形式输出二叉树：\n");
   PrintTree(T,0);
   printf("\n");
 
-  if(!DestroyBiTree(T))// 销毁二叉树T
-    printf("销毁失败!");
+  /*二叉树销毁*/
+  if(!DestroyBiTree(T))
+    printf("[!]二叉树T销毁失败!");
   else
-    printf("销毁成功!");
+    printf("[!]二叉树T销毁成功!");
   return 0;
 }
